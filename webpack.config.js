6df742095a8183504ea5455158bf6029eb18a65c
張育誠 <taco_chang@walsin.com>
2019-06-20 11:41:37 +0800

    update webpack config & .npmignore

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
   entry: {
-    'index': './src/index.js'
+    'index': ['@babel/polyfill', './src/index.js']
   },
   externals: {
     'react': 'commonjs react',
